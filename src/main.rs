33d53c8ba304a1806088f1950deac15e9b811853
Ye Yangchen <eikasia30@gmail.com>
2024-01-04 15:24:27 -0500

    Use Library's credential type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::{path::Path, time::Duration};
 
-use bili_api_rs::apis::live::user::{GetMedalForUserResponse, MedalItem};
+use bili_api_rs::{
+    apis::live::user::{GetMedalForUserResponse, MedalItem},
+    credential::Credential,
+};
 use clap::Parser;
 use cli::Cli;
 
@@ -8,64 +11,54 @@ mod cli;
 
 fn main() {
     let Cli { cookie, msg } = Cli::parse();
-    let cookie = Cookie::read_from_file(cookie.as_path());
-    let cookie_str = format!("SESSDATA={}", cookie.sessdata);
-    let medals = get_all_unlighted_medals(&cookie_str);
-    light_medals(&cookie, &cookie_str, &msg, &medals);
+    let cookie = read_cred_from_file(cookie.as_path());
+    let medals = get_all_unlighted_medals(&cookie);
+    light_medals(&cookie, &msg, &medals);
 }
 
-#[derive(Debug)]
-struct Cookie {
-    pub sessdata: String,
-    pub bili_jct: String,
-}
-
-impl Cookie {
-    fn read_from_file(path: impl AsRef<Path>) -> Self {
-        let file = std::fs::File::open(path.as_ref()).expect("Cannot open cookie file");
-        let value: serde_json::Value =
-            serde_json::from_reader(file).expect("Malformed cookie file");
-        let cookie_info = value.get("cookie_info").expect("Malformed cookie file");
-        let cookies = cookie_info.get("cookies").expect("Malformed cookie file");
-        let cookies = cookies.as_array().expect("Malformed cookie file");
-        let mut sessdata = None;
-        let mut bili_jct = None;
-        for entry in cookies {
-            let name = entry.get("name").expect("Malformed cookie file");
-            let name = name.as_str().expect("Malformed cookie file");
-            if name != "SESSDATA" && name != "bili_jct" {
-                continue;
-            }
-
-            let key = if name == "SESSDATA" {
-                &mut sessdata
-            } else {
-                &mut bili_jct
-            };
-            let value = entry.get("value").expect("Malformed cookie file");
-            let value = value.as_str().expect("Malformed cookie file");
-            *key = Some(value.to_string())
+fn read_cred_from_file(path: impl AsRef<Path>) -> Credential {
+    let file = std::fs::File::open(path.as_ref()).expect("Cannot open cookie file");
+    let value: serde_json::Value = serde_json::from_reader(file).expect("Malformed cookie file");
+    let cookie_info = value.get("cookie_info").expect("Malformed cookie file");
+    let cookies = cookie_info.get("cookies").expect("Malformed cookie file");
+    let cookies = cookies.as_array().expect("Malformed cookie file");
+    let mut sessdata = None;
+    let mut bili_jct = None;
+    for entry in cookies {
+        let name = entry.get("name").expect("Malformed cookie file");
+        let name = name.as_str().expect("Malformed cookie file");
+        if name != "SESSDATA" && name != "bili_jct" {
+            continue;
         }
 
-        if sessdata.is_none() || bili_jct.is_none() {
-            panic!("Malformed cookie file");
-        }
-
-        let sessdata = sessdata.unwrap();
-        let bili_jct = bili_jct.unwrap();
-
-        Cookie { sessdata, bili_jct }
+        let key = if name == "SESSDATA" {
+            &mut sessdata
+        } else {
+            &mut bili_jct
+        };
+        let value = entry.get("value").expect("Malformed cookie file");
+        let value = value.as_str().expect("Malformed cookie file");
+        *key = Some(value.to_string())
     }
+
+    if sessdata.is_none() || bili_jct.is_none() {
+        panic!("Malformed cookie file");
+    }
+
+    let sessdata = sessdata.unwrap();
+    let bili_jct = bili_jct.unwrap();
+
+    Credential::new(sessdata, bili_jct)
 }
 
-fn get_all_unlighted_medals(cookie_str: &str) -> Vec<MedalItem> {
+fn get_all_unlighted_medals(cookie: &Credential) -> Vec<MedalItem> {
     let mut medals = vec![];
     let agent = reqwest::blocking::Client::new();
     let mut cur_page = 1;
     let mut total_page = 10;
     while cur_page <= total_page {
         let response =
-            bili_api_rs::apis::live::user::get_medal_for_user(&agent, 10, cur_page, cookie_str)
+            bili_api_rs::apis::live::user::get_medal_for_user(&agent, 10, cur_page, cookie)
                 .expect("Failed to fetch user medal");
         match response {
             GetMedalForUserResponse::Success {
@@ -94,21 +87,13 @@ fn get_all_unlighted_medals(cookie_str: &str) -> Vec<MedalItem> {
     medals
 }
 
-fn light_medals(cookie: &Cookie, cookie_str: &str, msg: &str, medals: &[MedalItem]) {
+fn light_medals(cookie: &Credential, msg: &str, medals: &[MedalItem]) {
     let agent = reqwest::blocking::Client::new();
     for medal in medals {
         println!("正在点亮灯牌 [{}]...", &medal.medal_name);
         let room = medal.roomid;
         match bili_api_rs::apis::live::msg::send_live_message(
-            &agent,
-            room,
-            msg,
-            0xffffff,
-            25,
-            1,
-            1,
-            &cookie.bili_jct,
-            cookie_str,
+            &agent, room, msg, 0xffffff, 25, 1, 1, cookie,
         ) {
             Ok(_) => (),
             Err(e) => {
